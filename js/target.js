2051e962046164c70dd322b0206085af411debd1
David Rifkin & Jon Campbell & Kevin Perumal <pair+dlrifkin.heycampbell.kevinperumal@devbootcamp.com>
2015-01-20 17:08:47 -0500

    Change dimensions of target object

--- js/target.js
+++ js/target.js
@@ -1,11 +1,12 @@
 function Target(broom) {
   this.$broom = broom;
   this.x = this.$broom.width() /2;
-  this.y = this.$broom.height() /2;
-  this.width = 40;
-  this.height = 40;
+  this.y = this.$broom.height() /1.4  ;
+  this.width = 20;
+  this.height = 20;
   this.name = 'target';
   this.initDisplay()
+  // this.sprite = 'https://c1.staticflickr.com/5/4029/4280587485_aba01794df_z.jpg?zz=1'
 }
 
 ['initDisplay', 'updateDisplay'].forEach(function (func) {
